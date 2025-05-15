b92479e88a876c89422dccc11c80317376e6a9b0
Justine Gallien <justinegallien@gmail.com>
2025-05-15 18:27:10 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <header className="header">
+        <h1>Welcome to the Site!</h1>
+      </header>
+      <main className="main">
+        <h2> About Your Photographer</h2>
+        <p>
+          Hi, My name is Justine. I am a Certified Professional Photographer
+          based in beautiful Louisiana. I am driven by my passion for
+          photography and storytelling. As a proud mother of six amazing
+          children, my family is my greatest joy and the core of my creativity.
+          I truly understand the beautiful chaos and fleeting moments that make
+          family life so incredibly special. My own family is my biggest
+          inspiration. Witnessing their unique personalities blossom and
+          celebrating their milestones fuels my creativity and drives me to
+          create stunning images for my clients. Whether it's the joy of a
+          newborn's first smile, the playful energy of childhood, or the quiet
+          strength of a graduating senior, I strive to capture the genuine
+          emotions and authentic connections that make each moment unique. Like
+          many parents, I know how quickly kids grow up. It seems like one
+          moment they were taking their first steps, and then the next moment
+          they're learning to drive! That's why I'm so passionate about
+          capturing those precious, once-in-a-lifetime memories for other
+          families. I believe that photographs aren't just images; they're
+          tangible pieces of our history, telling the stories of our lives and
+          the love that connects us.
+        </p>
+        <q>I Came. I Saw. I Captured.</q>
+        <h2>My Photographic Journey</h2>
+        <p>
+          My love for photography ignited in 2011 with the purchase of my first
+          DSLR camera. Since then, I've dedicated myself to honing my skills and
+          mastering the art of visual storytelling. This dedication culminated
+          in achieving certification from the Professional Photographers of
+          America (PPA) in 2016. I'm honored to have received recognition for my
+          work from esteemed organizations including the Professional
+          Photographers of America (PPA), the Professional Photographers of
+          Louisiana (PPLA), the Southwest Professional Photographers Association
+          (SWPPA), and the Lafayette Photographic Society (LPS). Beyond my
+          personal growth, I believe in giving back to the photography
+          community. I've served on the Board of Directors for the PPLA and
+          actively participated in various committees, focusing on hospitality,
+          membership, exhibitions, and competitions.
+        </p>
+        <h2>Let's Create Memories Together!</h2>
+        <p>
+          Photography is more than just taking pictures; it's about capturing
+          emotions, preserving memories, and creating timeless art. As someone
+          who understands your vision and can capture the essence of your
+          family, I know how important it is to choose the right photographer. I
+          pride myself on creating a relaxed and enjoyable experience, ensuring
+          that everyone feels comfortable and confident in front of the camera.
+          I believe in the power of an image to tell a story and evoke feelings
+          that words often can't express. I invite you to join me on this
+          creative journey. Let me help you capture those fleeting moments, the
+          special connections, and the cherished memories that you'll treasure
+          for generations to come. Contact me today, and let's discuss how we
+          can create something beautiful together. Let's chat about how I can
+          help you preserve your family's precious memories for generations to
+          come. I'd love to hear your story!
+        </p>
+      </main>
+      <footer className="footer">
+        <strong>
+          &copy; <span>2025 </span> Justine Gallien All rights reserved.
+        </strong>
+      </footer>
+    </>
+  );
+}
+
+export default App;
